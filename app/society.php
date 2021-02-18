9d1a818a99e50b834ff89b198c9c0ca6f0b4a5a8
Cedric Lima <limacedric@hotmail.fr>
2021-02-18 16:25:36 +0100

    Creation migration society et modif migration user

---
+++ app/society.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class society extends Model
+{
+   /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+   */
+    protected $fillable = [
+      'name', 'email', 'phone', 'number','adress', 'city', 'code', 'description', 'logo'
+   ];
+}
